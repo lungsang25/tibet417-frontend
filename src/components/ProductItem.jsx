a7cdf6fd16320e7795c86972ea91e4e25b548ec1
Lungsangg <lungsang@esukhia.org>
2025-08-10 14:11:39 +0530

    image_fixed_size_update

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -8,8 +8,8 @@ const ProductItem = ({id,image,name,price}) => {
 
   return (
     <Link onClick={()=>scrollTo(0,0)} className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
-      <div className=' overflow-hidden'>
-        <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
+      <div className='overflow-hidden h-64 w-full'>
+        <img className='hover:scale-110 transition ease-in-out w-full h-full object-cover' src={image[0]} alt={name} />
       </div>
       <p className='pt-3 pb-1 text-sm'>{name}</p>
       <p className=' text-sm font-medium'>{currency}{price}</p>
